26ad9018fd4740a3e2982f0045ac3aede5050305
Joe Savona <savona@telmate.com>
2014-05-08 13:22:23 -0700

    working plc

--- src/capacitor.js
+++ src/capacitor.js
@@ -1,6 +1,7 @@
 var Promise = require('bluebird'),
     util = require('util'),
     EventEmitter = require('events').EventEmitter,
+    through = require('through'),
     Current = require('./current'),
     CapacitorApiError = require('./capacitor_api_error');
 
@@ -22,7 +23,8 @@ var Capacitor = function() {
 Capacitor.events = {
   INVALID_RUN: 'Capacitor.INVALID_RUN',
   WILL_RUN: 'Capacitor.WILL_RUN',
-  DID_RUN: 'Capacitor.DID_RUN'
+  DID_RUN: 'Capacitor.DID_RUN',
+  UNDEFINED_HANDLER: 'Capacitor.UNDEFINED_HANDLER'
 };
 
 // Capacitor instanceof EventEmitter
@@ -65,6 +67,29 @@ Capacitor.prototype.state = function(state) {
   this.states[state.name] = state;
 };
 
+
+/**
+ * initialize()
+ * @param  {String} stateName the state.name to start with
+ * @return {None}           no return value
+ *
+ * Initializes the system into the defined state. Call this *once* only.
+ * Throws errors if the stateName is invalid,
+ * if no such state exists, or if the system has already been initialized.
+ */
+Capacitor.prototype.initialize = function(stateName) {
+  if (typeof stateName !== 'string' || !stateName) {
+    throw new CapacitorApiError('Capacitor: initialize() accepts a string name of a state');
+  }
+  if (!(stateName in this.states)) {
+    throw new CapacitorApiError('Capacitor: initialize() state is undefined: ' + stateName);
+  }
+  if (this.current !== null) {
+    throw new CapacitorApiError(util.format('Capacitor: initialize() already called: current: %s new: %s', this.current.name, stateName));
+  }
+  this.current = this.states[stateName];
+};
+
 /**
  * run()
  * @param  {String} eventName name of the event to process
@@ -72,27 +97,50 @@ Capacitor.prototype.state = function(state) {
  * @return {None}           no return value
  *
  * Runs an event in the system - determines the current state and sends the event to the handler
- * defined there. if the state does not accept the event, emits `Capacitor.events.UNDEFINED_EVENT`.
+ * defined there. If the state does not accept the event, emits `Capacitor.events.UNDEFINED_HANDLER`.
  *
  * Event sequence:
  * - emit `WILL_RUN`
- * - if no handler: emit `UNDEFINED_EVENT`
+ * - if no handler: emit `UNDEFINED_HANDLER`
  * - if handler: (runs event)
  * - emit `DID_RUN`
  *   
  * If an error occurrs at any point in the above, `ERROR` is emitted. 
  */
 Capacitor.prototype.run = function(eventName, eventData) {
-  var flux;
+  var flux, stream;
+  current = new Current(eventName, eventData);
+  if (!this.current) {
+    this.emit(Capacitor.events.UNINITALIZED_ERROR, current);
+    return;
+  }
   if (typeof eventName !== 'string' || !eventName) {
     this.emit(Capacitor.events.INVALID_RUN, eventName, eventData);
     return;
   }
-  current = new Current(eventName, eventData);
-  this.emit(Capacitor.events.WILL_RUN, eventName, eventData);
+  this.emit(Capacitor.events.WILL_RUN, current);
 
+  if (typeof this.current[eventName] !== 'function') {
+    this.emit(Capacitor.events.UNDEFINED_HANDLER_ERROR, current);
+    return;
+  }
 
+  // create a stream to pipe events into
+  stream = through();
 
+  // pipe the current into the event handler
+  this.current[eventName].call(stream);
+
+  // write the current to the stream
+  stream.write(current);
+  // currently doing one event per stream instantiation
+  stream.end();
+  // catch errors
+  stream.on('error', function(err) {
+    this.emit(Capacitor.events.ERROR, this.current, current);
+  }.bind(this));
+
+  // cleanup: this should be the destination pipe of all streams.
   this.emit(Capacitor.events.DID_RUN, eventName, eventData);
 };
 
